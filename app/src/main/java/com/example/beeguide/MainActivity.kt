43a3d075c26a2fd5deb1f3cf118d7045751093b2
Felix-Caspar Himmelbauer <81152359+felix-caspar@users.noreply.github.com>
2024-01-10 22:54:23 +0100

    Beacon Monitoring and Ranging implemented

--- app/src/main/java/com/example/beeguide/MainActivity.kt
+++ app/src/main/java/com/example/beeguide/MainActivity.kt
@@ -13,15 +13,21 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
-import com.example.beeguide.navigation.MonitoringActivity
+import androidx.lifecycle.Observer
 import com.example.beeguide.ui.BeeGuideApp
 import com.example.beeguide.ui.screens.AppearanceViewModel
 import com.example.beeguide.ui.theme.BeeGuideTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.MonitorNotifier
+import org.altbeacon.beacon.Region
 
 class MainActivity : ComponentActivity() {
+
     //private val preferencesDataStore by lazy { PreferencesDataStore(this) }
 
     private lateinit var bluetoothAdapter: BluetoothAdapter
@@ -30,6 +36,7 @@ class MainActivity : ComponentActivity() {
     companion object {
         private const val REQUEST_ENABLE_BT = 1
         private const val REQUEST_LOCATION_PERMISSION = 2
+        private val TAG = "MainActivity"
     }
 
     private val appearanceViewModel by viewModels<AppearanceViewModel>(
@@ -72,7 +79,8 @@ class MainActivity : ComponentActivity() {
 
 
 
-        val intent = Intent(this, MonitoringActivity::class.java)
+        //val intent = Intent(this, MonitoringActivity::class.java)
+        //startActivity(intent)
 
         CoroutineScope(Dispatchers.IO).launch {
             Log.d("AppearanceViewModel", "create: ${preferencesDataStore.getDarkThemeMode()}")
@@ -84,6 +92,51 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+        Log.d(TAG, "Heloo")
+        startBeaconMonitor()
+    }
+
+    val monitoringObserver = Observer<Int> {state ->
+        if (state == MonitorNotifier.OUTSIDE) {
+            Log.d(TAG, "outside beacon region: ")
+        }
+        else {
+            Log.d(TAG, "inside beacon region: ")
+
+        }
+    }
+
+    private fun startBeaconMonitor(){
+        val beaconManager =  BeaconManager.getInstanceForApplication(this)
+
+        // The example shows how to find iBeacon.
+        val parser = BeaconParser().
+            setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        parser.setHardwareAssistManufacturerCodes(arrayOf(0x004c).toIntArray())
+        beaconManager.beaconParsers.add(
+            parser)
+
+
+        val region = Region("all-beacons", null, null, null)
+        // Set up a Live Data observer for beacon data
+        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(region)
+        // observer will be called each time the monitored regionState changes (inside vs. outside region)
+
+        regionViewModel.rangedBeacons.observe(this){beacons->
+            val rangeAgeMillis = System.currentTimeMillis() - (beacons.firstOrNull()?.lastCycleDetectionTimestamp ?: 0)
+            if (rangeAgeMillis < 10000) {
+                Log.d(MainActivity.TAG, "Ranged: ${beacons.count()} beacons")
+                for (beacon: Beacon in beacons) {
+                    Log.d(TAG, "$beacon about ${beacon.distance} meters away")
+                }
+            }
+            else {
+                Log.d(MainActivity.TAG, "Ignoring stale ranged beacons from $rangeAgeMillis millis ago")
+            }
+        }
+        regionViewModel.regionState.observe(this, monitoringObserver)
+        beaconManager.startMonitoring(region)
+        beaconManager.startRangingBeacons(region)
     }
 
     private fun checkLocationPermission() {
--- app/src/main/java/com/example/beeguide/navigation/MonitoringActivity.kt
+++ app/src/main/java/com/example/beeguide/navigation/MonitoringActivity.kt
@@ -1,11 +1,13 @@
 package com.example.beeguide.navigation
 
 import android.app.Activity
+import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
@@ -16,30 +18,34 @@ import androidx.lifecycle.Observer
 import org.altbeacon.beacon.AltBeaconParser.TAG
 import org.altbeacon.beacon.MonitorNotifier
 
-class MonitoringActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("navDebug", "Monitoring started")
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // TODO: Add code here to obtain location permission from user
-        // TODO: Add beaconParsers for any properietry beacon formats you wish to detect
-
-        val beaconManager =  BeaconManager.getInstanceForApplication(this)
-        val region = Region("all-beacons-region", null, null, null)
-        // Set up a Live Data observer so this Activity can get monitoring callbacks
-        // observer will be called each time the monitored regionState changes (inside vs. outside region)
-        beaconManager.getRegionViewModel(region).regionState.observe(this, monitoringObserver)
-        beaconManager.startMonitoring(region)
+class BeaconMonitoringService : Service() {
+    override fun onCreate() {
+//        Log.d("navDebug", "Monitoring started")
+//
+//        // TODO: Add beaconParsers for any properietry beacon formats you wish to detect
+//
+//        val beaconManager =  BeaconManager.getInstanceForApplication(this)
+//        val region = Region("all-beacons-region", null, null, null)
+//        // Set up a Live Data observer so this Activity can get monitoring callbacks
+//        // observer will be called each time the monitored regionState changes (inside vs. outside region)
+//        beaconManager.getRegionViewModel(region).regionState.observe(monitoringObserver)
+//        beaconManager.startMonitoring(region)
     }
-
 
     val monitoringObserver = Observer<Int> { state ->
         if (state == MonitorNotifier.INSIDE) {
-            Log.d("beacon", "Detected beacons(s)")
+            Log.d(TAG, "Detected beacons(s)")
         }
         else {
-            Log.d("beacon", "Stopped detecteing beacons")
+            Log.d(TAG, "Stopped detecteing beacons")
         }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
     }
 }
