eaf21610bc97ec7a9fb7f5efc64d51cdd788427a
Oskar <oskar@obernberger.at>
2024-01-29 14:47:45 +0100

    profilescreen ui

--- app/src/main/java/com/example/beeguide/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/beeguide/ui/screens/ProfileScreen.kt
@@ -1,9 +1,16 @@
 package com.example.beeguide.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import com.example.beeguide.R
 import com.example.beeguide.ui.viewmodels.UserUiState
 
 @Composable
@@ -15,14 +22,22 @@ fun ProfileScreen(
         is UserUiState.Loading ->
             Text(text = "Loading...")
 
-        is UserUiState.Success ->
+        is UserUiState.Success -> {
+            Icon(
+                painter = painterResource(R.drawable.round_shape),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.surface,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .scale(1.3f)
+            )
             Column {
                 Text(text = "${userUiState.user.firstName} ${userUiState.user.lastName}")
                 Button(onClick = onSettingsButtonClicked) {
                     Text(text = "Settings")
                 }
             }
-
+        }
 
         else -> Text(text = "Error!")
     }
