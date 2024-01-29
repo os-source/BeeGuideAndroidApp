a30f38b6571d7de11fa88c493c2ee5b191faca6f
Oskar <oskar@obernberger.at>
2024-01-29 14:46:51 +0100

    new color scheme

--- app/src/main/java/com/example/beeguide/ui/theme/Color.kt
+++ app/src/main/java/com/example/beeguide/ui/theme/Color.kt
@@ -5,11 +5,11 @@ import androidx.compose.ui.graphics.Color
 val light = Color(0xFFF3F3F3)
 val dark = Color(0xFF161616)
 
-val background_dark = Color(0xFF1B2022)
-val surface_dark = Color(0xFF212629)
+val background_dark = Color(0xFF0D1013)
+val surface_dark = Color(0xFF14181D)
 
-val background_light = Color(0xFFC5D9F0)
-val surface_light = Color(0xFFA3BEDB)
+val background_light = Color(0xFFEFF2F5)
+val surface_light = Color(0xFFE4E9EE)
 
 val primary = Color(0xFF16588E)
 val primary_light = Color(0xFF757DE8)
