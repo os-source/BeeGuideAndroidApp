d543056772a5a8174ea94a468bbce111931d3b7e
Felix-Caspar Himmelbauer <81152359+felix-caspar@users.noreply.github.com>
2024-01-29 14:12:27 +0100

    Implemented calculating intersections

--- app/src/main/java/com/example/beeguide/navigation/algorithm/CalculationController.kt
+++ app/src/main/java/com/example/beeguide/navigation/algorithm/CalculationController.kt
@@ -1,13 +1,41 @@
 package com.example.beeguide.navigation.algorithm
 
 import android.util.Log
-import com.example.beeguide.ui.viewmodels.MapViewModel
-import org.altbeacon.beacon.RegionViewModel
 
 class CalculationController(private val circles: List<Circle>) {
-    fun log(){
-        circles.forEach{
-            Log.d("Circle", "\nCircle:\nX: ${it.x}\nY: ${it.y}\nRadius: ${it.radius}\n")
+    private var circleDuos = mutableListOf<kotlin.Array<Circle>>()
+    private var intersections = mutableListOf<Point>()
+
+    fun control(){
+        generateCircleDuos()
+        val intersectionCalculator = IntersectionCalculator()
+
+        circleDuos.forEach { duo ->
+            intersectionCalculator.getIntersections(duo[0], duo[1])
+                ?.forEach { intersection -> intersections.add(intersection) }
         }
+    }
+
+    fun logPoints(){
+        intersections.forEach{point -> Log.d("Point", "Point-Coordinates: X: ${point.x} Y: ${point.y}")}
+    }
+
+    private fun generateCircleDuos(){
+        circles.forEach{ circle1 ->
+            circles.forEach{circle2 ->
+                if(circle1 != circle2){
+                    val circleArr: kotlin.Array<Circle>
+                    if(circle1.radius < circle2.radius){
+                        circleArr = arrayOf(circle1, circle2)
+                    }
+                    else
+                    {
+                        circleArr = arrayOf(circle2, circle1)
+                    }
+                    circleDuos.add(circleArr)
+                }
+            }
+        }
+        circleDuos = circleDuos.distinct().toMutableList()
     }
 }
--- app/src/main/java/com/example/beeguide/navigation/algorithm/IntersectionCalculator.kt
+++ app/src/main/java/com/example/beeguide/navigation/algorithm/IntersectionCalculator.kt
@@ -8,59 +8,56 @@ import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.math.sqrt
 
-class IntersectionCalculator(beacons: RegionViewModel, map: MapViewModel) {
+class IntersectionCalculator() {
+    fun getIntersections(circle1: Circle, circle2: Circle) : Array<Point>? {
 
-    fun getIntersections(){
-        fun circleCircleIntersectionPoints(circle1: Circle, circle2: Circle) : Array<Point>? {
+        val r: Double; val R: Double; val d: Double; val dx: Double; val dy: Double
+        val cx: Double; val cy: Double; val Cx: Double; val Cy: Double
+        val eps = 0.0000000000001
 
-            val r: Double; val R: Double; val d: Double; val dx: Double; val dy: Double
-            val cx: Double; val cy: Double; val Cx: Double; val Cy: Double
-            val eps = 0.00000000001
-
-            if (circle1.radius < circle2.radius) {
-                r  = circle1.radius.toDouble();  R = circle2.radius.toDouble()
-                cx = circle1.x.toDouble(); cy = circle1.y.toDouble()
-                Cx = circle2.x.toDouble(); Cy = circle2.y.toDouble()
-            } else {
-                r  = circle2.radius.toDouble(); R  = circle1.radius.toDouble();
-                Cx = circle1.x.toDouble(); Cy = circle1.y.toDouble()
-                cx = circle2.x.toDouble(); cy = circle2.y.toDouble()
-            }
-
-            // Compute the vector <dx, dy>
-            dx = cx - Cx
-            dy = cy - Cy
-
-            // pythagoras
-            d = sqrt( dx * dx + dy * dy )
-
-            // There are an infinite number of solutions
-            // Seems appropriate to also return null
-            if (d < eps && abs(R-r) < eps) return null
-
-            // No intersection (circles centered at the
-            // same place with different size)
-            else if (d < eps) return null
-
-            val x = (dx / d) * R + Cx
-            val y = (dy / d) * R + Cy
-            val P = Point(x.toInt(), y.toInt())
-
-            // Single intersection (kissing circles)
-            if (abs((R+r)-d) < eps || abs(R-(r+d)) < eps) return arrayOf(P)
-
-            // No intersection. Either the small circle contained within
-            // big circle or circles are simply disjoint.
-            if ( (d+r) < R || (R+r < d) ) return null
-
-            val C = Point(Cx.toInt(), Cy.toInt())
-            val angle = acossafe((r*r-d*d-R*R)/(-2.0*d*R))
-            val pt1: Point = rotatePoint(C, P, +angle)
-            val pt2: Point = rotatePoint(C, P, -angle)
-            return arrayOf<Point>(pt1, pt2)
-
+        if (circle1.radius < circle2.radius) {
+            r  = circle1.radius.toDouble();  R = circle2.radius.toDouble()
+            cx = circle1.x.toDouble(); cy = circle1.y.toDouble()
+            Cx = circle2.x.toDouble(); Cy = circle2.y.toDouble()
+        } else {
+            r  = circle2.radius.toDouble(); R  = circle1.radius.toDouble();
+            Cx = circle1.x.toDouble(); Cy = circle1.y.toDouble()
+            cx = circle2.x.toDouble(); cy = circle2.y.toDouble()
         }
+
+        // Compute the vector <dx, dy>
+        dx = cx - Cx
+        dy = cy - Cy
+
+        // pythagoras
+        d = sqrt( dx * dx + dy * dy )
+
+        // There are an infinite number of solutions
+        // Seems appropriate to also return null
+        if (d < eps && abs(R-r) < eps) return null
+
+        // No intersection (circles centered at the
+        // same place with different size)
+        else if (d < eps) return null
+
+        val x = (dx / d) * R + Cx
+        val y = (dy / d) * R + Cy
+        val P = Point(x.toInt(), y.toInt())
+
+        // Single intersection (kissing circles)
+        if (abs((R+r)-d) < eps || abs(R-(r+d)) < eps) return arrayOf(P)
+
+        // No intersection. Either the small circle contained within
+        // big circle or circles are simply disjoint.
+        if ( (d+r) < R || (R+r < d) ) return null
+
+        val C = Point(Cx.toInt(), Cy.toInt())
+        val angle = acossafe((r*r-d*d-R*R)/(-2.0*d*R))
+        val pt1: Point = rotatePoint(C, P, +angle)
+        val pt2: Point = rotatePoint(C, P, -angle)
+        return arrayOf<Point>(pt1, pt2)
     }
+
 
     fun acossafe(x: Double): Double {
         if (x >= +1.0) return 0.0
