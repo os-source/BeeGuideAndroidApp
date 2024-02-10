e8b27d291dbf93e86e0436121c62b273b25a3a6d
prushel <98187763+prushel@users.noreply.github.com>
2024-02-10 18:59:47 +0100

    API Interface for Login

--- app/src/main/java/com/example/beeguide/data/BeeGuideRespository.kt
+++ app/src/main/java/com/example/beeguide/data/BeeGuideRespository.kt
@@ -1,22 +1,37 @@
 package com.example.beeguide.data
 
+import com.example.beeguide.model.Login
+import com.example.beeguide.model.LoginRequest
 import com.example.beeguide.model.Map
 import com.example.beeguide.model.User
 import com.example.beeguide.network.BeeGuideApiService
 
 interface BeeGuideRespository {
     suspend fun getMap(): Map
+    suspend fun Login(email: String, password: String, remember: Boolean)
     suspend fun getUser(): User
 
 
 }
 
 class NetworkBeeGuideRepository(
-    private val beeGuideApiService: BeeGuideApiService
+    private val beeGuideApiService: BeeGuideApiService, private val authenticator : AuthenticationManager
 ) : BeeGuideRespository {
     override suspend fun getMap(): Map = beeGuideApiService.getMap()
 
     override suspend fun getUser() : User = beeGuideApiService.getUser()
+
+    override suspend fun Login(email:String, password:String, remember: Boolean)
+    {
+        val token:Login = beeGuideApiService.login(LoginRequest(email, password), false);
+        authenticator.saveJWTToken(token.JWT);
+        if(remember)
+        {
+            authenticator.saveRefreshToken(token.refresh);
+        }
+
+
+    }
 
 
 }
--- app/src/main/java/com/example/beeguide/network/BeeGuideApiService.kt
+++ app/src/main/java/com/example/beeguide/network/BeeGuideApiService.kt
@@ -1,9 +1,14 @@
 package com.example.beeguide.network
 
+import com.example.beeguide.model.Login
+import com.example.beeguide.model.LoginRequest
 import com.example.beeguide.model.Map
 import com.example.beeguide.model.User
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BeeGuideApiService {
     @GET("regions/426C7565-4368-6172-6D43-6561636F6E73/maps/3000")
@@ -12,6 +17,14 @@ interface BeeGuideApiService {
     @GET("/user")
     suspend fun getUser(): User
     //getMapXML as String
+
+
+    @POST("login?rememberme={remember}")
+    suspend fun login(
+            @Body body: LoginRequest,
+            @Query("rememberme") remember : Boolean
+
+        ): Login
 
     @GET("/maps/{id}/file")
     suspend fun getMapFile(@Path("id") map_id: Int): String
