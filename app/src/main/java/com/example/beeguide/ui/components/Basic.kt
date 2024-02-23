f6345c1def7d78ff7a4925b9b0b6a52bb7265471
Oskar <oskar@obernberger.at>
2024-02-23 22:09:57 +0100

    add circular progress indecator composable

--- app/src/main/java/com/example/beeguide/ui/components/Basic.kt
+++ app/src/main/java/com/example/beeguide/ui/components/Basic.kt
@@ -1,2 +1,18 @@
 package com.example.beeguide.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BeeGuideCircularProgressIndicator() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
